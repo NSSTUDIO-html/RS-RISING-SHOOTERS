c1144e851069be567f4f3e34a83141b32b918c9d
NAGA SASANK <rockingshooters7@gmail.com>
2025-07-17 14:18:19 +0530

    Update ig.js
    Signed-off-by: NAGA SASANK  <rockingshooters7@gmail.com>

--- ig.js
+++ ig.js
@@ -4,7 +4,7 @@ import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.158.0/build/three.m
     const scene = new THREE.Scene();
   scene.background = new THREE.Color( 0x444442 );
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    const renderer = new THREE.WebGLRenderer();
+    //const renderer = new THREE.WebGLRenderer();
    // renderer.setSize(window.innerWidth, window.innerHeight);
     //document.body.appendChild(renderer.domElement);
 
